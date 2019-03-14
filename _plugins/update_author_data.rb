acc49b7238a9e14c878b895f6e514404ad51cc11
JacOng17 <jacob_ong88@hotmail.com>
2019-03-15 10:28:40 +1300

    Fixed CodeClimate issues.

--- _plugins/update_author_data.rb
+++ _plugins/update_author_data.rb
@@ -4,7 +4,7 @@
 # Detects if authors have published posts
 # And renders a page for them accordingly
 # at /author/:name/
-Jekyll::Hooks.register :site, :after_init do 
+Jekyll::Hooks.register :site, :after_init do
   author_data = SiteData::AuthorData.new
 
   author_data.published_authors.each do |author|
