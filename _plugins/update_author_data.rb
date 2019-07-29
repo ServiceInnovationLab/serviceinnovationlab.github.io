6a3241e41c227957b0048a1893c24df42d11467b
Jacob <jacob.sien.ong@gmail.com>
2019-07-29 16:13:43 +1200

    Commented out code which causes files to be updated upon restarting server.

--- _plugins/update_author_data.rb
+++ _plugins/update_author_data.rb
@@ -4,14 +4,15 @@
 # Detects if authors have published posts
 # And renders a page for them accordingly
 # at /author/:name/
-Jekyll::Hooks.register :site, :after_init do
-  author_data = SiteData::AuthorData.new
 
-  author_data.published_authors.each do |author|
-    author_data.update(author, 'published', true)
-  end
+# Jekyll::Hooks.register :site, :after_init do
+#   author_data = SiteData::AuthorData.new
 
-  author_data.unpublished_authors.each do |author|
-    author_data.update(author, 'published', false)
-  end
-end
+#   author_data.published_authors.each do |author|
+#     author_data.update(author, 'published', true)
+#   end
+
+#   author_data.unpublished_authors.each do |author|
+#     author_data.update(author, 'published', false)
+#   end
+# end
