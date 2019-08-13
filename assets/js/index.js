b9aab87a30c857ac9ab87657305fce3c3151a291
Deployment step on circle-ci <labplus@dia.govt.nz>
2019-08-13 21:51:03 +0000

    Build 2835

--- assets/js/index.js
+++ assets/js/index.js
@@ -52,10 +52,6 @@ $(function() {
     },
   };
 
-  $form.ajaxChimp({
-    callback: callbackFunction,
-  });
-
   function callbackFunction(resp) {
     if (resp.result === 'success') {
       newsletterForm.response();
---
+++ vendor/bundle/ruby/2.6.0/gems/jekyll_pages_api_search-0.5.0/lib/jekyll_pages_api_search/version.rb
@@ -0,0 +1,3 @@
+module JekyllPagesApiSearch
+  VERSION = '0.5.0'
+end
