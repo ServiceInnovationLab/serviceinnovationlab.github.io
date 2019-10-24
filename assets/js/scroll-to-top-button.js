b057110d0a101fd1b3504132c2521d2966d402cf
JacOng17 <JacOng17@users.noreply.github.com>
2019-10-25 12:52:44 +1300

    Update assets/js/scroll-to-top-button.js
    Co-Authored-By: Brenda Wallace <brenda.wallace@dia.govt.nz>

--- assets/js/scroll-to-top-button.js
+++ assets/js/scroll-to-top-button.js
@@ -1,5 +1,5 @@
 //Get the button:
-mybutton = document.getElementById("myBtn");
+var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
