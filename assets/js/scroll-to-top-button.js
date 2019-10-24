92138df889c7dc4ad77995082023112908f28aa5
codefactor-io <support@codefactor.io>
2019-10-24 23:57:02 +0000

    [CodeFactor] Apply fixes

--- assets/js/scroll-to-top-button.js
+++ assets/js/scroll-to-top-button.js
@@ -1,8 +1,10 @@
-//Get the button:
+// Get the button:
 var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+scrollFunction()
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
