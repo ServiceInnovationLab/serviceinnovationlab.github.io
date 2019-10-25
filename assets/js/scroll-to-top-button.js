415b0666139d57572a710bcd18ccbec8231baada
Jacob <jacob.sien.ong@gmail.com>
2019-10-25 13:07:06 +1300

    Renamed class to id & added the eslint-env to ignore code.

--- assets/js/scroll-to-top-button.js
+++ assets/js/scroll-to-top-button.js
@@ -1,8 +1,12 @@
-//Get the button:
-var mybutton = document.getElementById("myBtn");
+/* eslint-env browser */
+
+// Get the button:
+var mybutton = document.getElementById("scrollToTopBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+  scrollFunction()
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
