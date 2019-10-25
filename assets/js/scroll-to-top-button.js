ba4388f320af24dfc0097e6e31f892fcd1bb4f35
Jacob <jacob.sien.ong@gmail.com>
2019-10-25 14:27:02 +1300

    Added handler to the onclick function.

--- assets/js/scroll-to-top-button.js
+++ assets/js/scroll-to-top-button.js
@@ -17,7 +17,7 @@ function scrollFunction() {
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+mybutton.onclick = function() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
