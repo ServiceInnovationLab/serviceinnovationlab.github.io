f13d94ef8e60713ba7f4f46f5ce9f4ec0d3fa27e
JacOng17 <JacOng17@users.noreply.github.com>
2019-10-25 14:20:01 +1300

    Update assets/js/scroll-to-top-button.js

--- assets/js/scroll-to-top-button.js
+++ assets/js/scroll-to-top-button.js
@@ -8,7 +8,6 @@ window.onscroll = function() {
   scrollFunction()
 };
 
-topFunction();
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
