7b77068dadf70dafc04160ca77b774ceb0b1e252
JacOng17 <JacOng17@users.noreply.github.com>
2019-10-25 14:20:21 +1300

    Update assets/js/scroll-to-top-button.js

--- assets/js/scroll-to-top-button.js
+++ assets/js/scroll-to-top-button.js
@@ -8,7 +8,6 @@ window.onscroll = function() {
   scrollFunction()
 };
 
-
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
