1dec45770a05c4fdf9f5e3e1d766427bfe7f2635
JacOng17 <jacob_ong88@hotmail.com>
2020-03-12 11:39:44 +1300

    Merge.

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -1,6 +1,5 @@
 $(function() {
   window.sr = ScrollReveal();
-<<<<<<< HEAD
 	if ($(window).width() < 768) {
 		if ($('.timeline-content').hasClass('js--fadeInLeft')) {
 			$('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
@@ -19,26 +18,6 @@ $(function() {
 			easing: 'ease-in-out',
 			duration: 800,
 		});
-=======
-  if ($(window).width() < 768) {
-  	if ($('.timeline-content').hasClass('js--fadeInLeft')) {
-  		$('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
-  	}
-  	sr.reveal('.js--fadeInRight', {
-	    origin: 'right',
-	    distance: '300px',
-	    easing: 'ease-in-out',
-	    duration: 800,
-	  });
-
-  } else {
-   	sr.reveal('.js--fadeInLeft', {
-	    origin: 'left',
-	    distance: '300px',
-		  easing: 'ease-in-out',
-	    duration: 800,
-	  });
->>>>>>> 4f61cd8cffa343497290ed51991b61ce450df94e
 
 		sr.reveal('.js--fadeInRight', {
 			origin: 'right',
