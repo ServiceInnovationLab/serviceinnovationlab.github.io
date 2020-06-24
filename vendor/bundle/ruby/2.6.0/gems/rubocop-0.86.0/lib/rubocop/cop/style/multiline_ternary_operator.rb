b13d1c1d8cbde0f040f5754632e2cec17b7e70f8
Deployment step on circle-ci <labplus@dia.govt.nz>
2020-06-24 01:48:06 +0000

    Build 9681

---
+++ vendor/bundle/ruby/2.6.0/gems/rubocop-0.86.0/lib/rubocop/cop/style/multiline_ternary_operator.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module Style
+      # This cop checks for multi-line ternary op expressions.
+      #
+      # @example
+      #   # bad
+      #   a = cond ?
+      #     b : c
+      #   a = cond ? b :
+      #       c
+      #   a = cond ?
+      #       b :
+      #       c
+      #
+      #   # good
+      #   a = cond ? b : c
+      #   a = if cond
+      #     b
+      #   else
+      #     c
+      #   end
+      class MultilineTernaryOperator < Cop
+        MSG = 'Avoid multi-line ternary operators, ' \
+              'use `if` or `unless` instead.'
+
+        def on_if(node)
+          return unless node.ternary? && node.multiline?
+
+          add_offense(node)
+        end
+
+        def autocorrect(node)
+          lambda do |corrector|
+            corrector.replace(node, <<~RUBY.chop)
+              if #{node.condition.source}
+                #{node.if_branch.source}
+              else
+                #{node.else_branch.source}
+              end
+            RUBY
+          end
+        end
+      end
+    end
+  end
+end
